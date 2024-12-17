b902a0e16d891e125bd431932192a558350cefd1
loweezzt <lowe.vesterberg@elev.ga.ntig.se>
2024-12-17 15:18:12 +0100

    Fixat snygg bakgrund med nice färg.

--- js/Particle.js
+++ js/Particle.js
@@ -2,10 +2,10 @@ class Particle {
   constructor(x, y, color) {
     this.x = x;
     this.y = y;
-    this.dy = (1 + Math.random() * 3) * speed / 5;
-    this.dx = (-1 + Math.random() * 2) * speed / 5;
-    this.color = `rgba(${color[0]}, ${color[1]}, ${color[2]}, ${Math.random()})`;
-    this.size = 2 + Math.floor(Math.random() * 2);
+    this.dy = (1 + Math.random() * 10) * speed / 3;
+    this.dx = (-5 + Math.random() * 10) * speed / 2;
+    this.color = `rgba(${color[1]}, ${color[2]}, ${color[2]}, ${Math.random()})`;
+    this.size = 4 + Math.floor(Math.random(10) * 4);
     this.toDelete = false;
   }
 
